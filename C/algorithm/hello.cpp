0b98580e2aa2834e4b01b81e8be52cfd6d13b112
HuYi <y-hu17@mails.tsinghua.edu.cn>
2020-03-05 15:23:13 +0800

    0

--- C/algorithm/hello.cpp
+++ C/algorithm/hello.cpp
@@ -3,13 +3,13 @@
  * @Author: HuYi
  * @Date: 2020-03-05 10:59:15
  * @LastEditors: HuYi
- * @LastEditTime: 2020-03-05 12:31:25
+ * @LastEditTime: 2020-03-05 15:22:06
  */
 #include <iostream>
 using namespace std;
 int main()
 {
-    cout << "cnm!" << endl;
+    cout << "helloworld" << endl;
     system("pause");
     return 0;
 }
