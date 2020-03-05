42ac8ac1a3dd5d60845bae23f74442a8ed43cd68
HuYi <y-hu17@mails.tsinghua.edu.cn>
2020-03-05 20:57:06 +0800

    0

---
+++ C/test.cpp
@@ -0,0 +1,15 @@
+/*
+ * @Description: 
+ * @Author: HuYi
+ * @Date: 2020-03-05 20:53:32
+ * @LastEditors: HuYi
+ * @LastEditTime: 2020-03-05 20:53:47
+ */
+#include <stdio.h>
+#include <windows.h>
+int main()
+{
+    printf("Hello World\n");
+    system("pause");
+    return 0;
+}
