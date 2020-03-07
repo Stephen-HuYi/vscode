9e955800fc8a43b1872d03a9340cd407634e0422
HuYi <y-hu17@mails.tsinghua.edu.cn>
2020-03-07 12:18:36 +0800

    0

--- C/algorithm/hw3/strassen.cpp
+++ C/algorithm/hw3/strassen.cpp
@@ -3,7 +3,7 @@
  * @Author: HuYi
  * @Date: 2020-03-06 08:32:14
  * @LastEditors: HuYi
- * @LastEditTime: 2020-03-07 12:09:06
+ * @LastEditTime: 2020-03-07 12:17:58
  */
 #include <iostream>
 #include <windows.h>
@@ -13,44 +13,56 @@ template <typename T>
 class Mat
 {
 public:
-    void ADD(T **A, T **B, T **Res, int Size);
-    void SUB(T **A, T **B, T **Res, int Size);
-    void MUL(T **A, T **B, T **Res, int Size); //朴素算法实现
-    void Fill(T **A, T **B, int Len);          //A,B矩阵赋值
-    void Print(T **A, int Size);               //打印矩阵
+    void Fill(T **A, T **B, int N);            //A,B矩阵赋值
+    void ADD(T **A, T **B, T **Res, int N);    //矩阵加法
+    void SUB(T **A, T **B, T **Res, int N);    //矩阵减法
+    void MUL(T **A, T **B, T **Res, int N);    //矩阵乘法brute
+    void Print(T **A, int N);                  //打印矩阵
     void Strassen(int N, T **A, T **B, T **C); //strassen算法实现
 };
 template <typename T>
-void Mat<T>::ADD(T **A, T **B, T **Res, int Size)
+void Mat<T>::Fill(T **A, T **B, int N)
 {
-    for (int i = 0; i < Size; i++)
+    for (int i = 0; i < N; i++)
     {
-        for (int j = 0; j < Size; j++)
+        for (int j = 0; j < N; j++)
+        {
+            A[i][j] = rand() % 5;
+            B[i][j] = rand() % 5;
+        }
+    }
+}
+template <typename T>
+void Mat<T>::ADD(T **A, T **B, T **Res, int N)
+{
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < N; j++)
         {
             Res[i][j] = A[i][j] + B[i][j];
         }
     }
 }
 template <typename T>
-void Mat<T>::SUB(T **A, T **B, T **Res, int Size)
+void Mat<T>::SUB(T **A, T **B, T **Res, int N)
 {
-    for (int i = 0; i < Size; i++)
+    for (int i = 0; i < N; i++)
     {
-        for (int j = 0; j < Size; j++)
+        for (int j = 0; j < N; j++)
         {
             Res[i][j] = A[i][j] - B[i][j];
         }
     }
 }
 template <typename T>
-void Mat<T>::MUL(T **A, T **B, T **Res, int Size)
+void Mat<T>::MUL(T **A, T **B, T **Res, int N)
 {
-    for (int i = 0; i < Size; i++)
+    for (int i = 0; i < N; i++)
     {
-        for (int j = 0; j < Size; j++)
+        for (int j = 0; j < N; j++)
         {
             Res[i][j] = 0;
-            for (int k = 0; k < Size; k++)
+            for (int k = 0; k < N; k++)
             {
                 Res[i][j] += A[i][k] * B[k][j];
             }
@@ -102,51 +114,51 @@ void Mat<T>::Strassen(int N, T **A, T **B, T **C)
         T **c11, **c12, **c21, **c22;
         T **m1, **m2, **m3, **m4, **m5, **m6, **m7;
         T **resa, **resb;
-        int halfsize = n / 2;
-        a11 = new T *[halfsize];
-        a12 = new T *[halfsize];
-        a21 = new T *[halfsize];
-        a22 = new T *[halfsize];
-        b11 = new T *[halfsize];
-        b12 = new T *[halfsize];
-        b21 = new T *[halfsize];
-        b22 = new T *[halfsize];
-        c11 = new T *[halfsize];
-        c12 = new T *[halfsize];
-        c21 = new T *[halfsize];
-        c22 = new T *[halfsize];
-        m1 = new T *[halfsize];
-        m2 = new T *[halfsize];
-        m3 = new T *[halfsize];
-        m4 = new T *[halfsize];
-        m5 = new T *[halfsize];
-        m6 = new T *[halfsize];
-        m7 = new T *[halfsize];
-        resa = new T *[halfsize];
-        resb = new T *[halfsize];
-        for (int i = 0; i < halfsize; i++)
+        int n_half = n / 2;
+        a11 = new T *[n_half];
+        a12 = new T *[n_half];
+        a21 = new T *[n_half];
+        a22 = new T *[n_half];
+        b11 = new T *[n_half];
+        b12 = new T *[n_half];
+        b21 = new T *[n_half];
+        b22 = new T *[n_half];
+        c11 = new T *[n_half];
+        c12 = new T *[n_half];
+        c21 = new T *[n_half];
+        c22 = new T *[n_half];
+        m1 = new T *[n_half];
+        m2 = new T *[n_half];
+        m3 = new T *[n_half];
+        m4 = new T *[n_half];
+        m5 = new T *[n_half];
+        m6 = new T *[n_half];
+        m7 = new T *[n_half];
+        resa = new T *[n_half];
+        resb = new T *[n_half];
+        for (int i = 0; i < n_half; i++)
         {
-            a11[i] = new T[halfsize];
-            a12[i] = new T[halfsize];
-            a21[i] = new T[halfsize];
-            a22[i] = new T[halfsize];
-            b11[i] = new T[halfsize];
-            b12[i] = new T[halfsize];
-            b21[i] = new T[halfsize];
-            b22[i] = new T[halfsize];
-            c11[i] = new T[halfsize];
-            c12[i] = new T[halfsize];
-            c21[i] = new T[halfsize];
-            c22[i] = new T[halfsize];
-            m1[i] = new T[halfsize];
-            m2[i] = new T[halfsize];
-            m3[i] = new T[halfsize];
-            m4[i] = new T[halfsize];
-            m5[i] = new T[halfsize];
-            m6[i] = new T[halfsize];
-            m7[i] = new T[halfsize];
-            resa[i] = new T[halfsize];
-            resb[i] = new T[halfsize];
+            a11[i] = new T[n_half];
+            a12[i] = new T[n_half];
+            a21[i] = new T[n_half];
+            a22[i] = new T[n_half];
+            b11[i] = new T[n_half];
+            b12[i] = new T[n_half];
+            b21[i] = new T[n_half];
+            b22[i] = new T[n_half];
+            c11[i] = new T[n_half];
+            c12[i] = new T[n_half];
+            c21[i] = new T[n_half];
+            c22[i] = new T[n_half];
+            m1[i] = new T[n_half];
+            m2[i] = new T[n_half];
+            m3[i] = new T[n_half];
+            m4[i] = new T[n_half];
+            m5[i] = new T[n_half];
+            m6[i] = new T[n_half];
+            m7[i] = new T[n_half];
+            resa[i] = new T[n_half];
+            resb[i] = new T[n_half];
         }
         //splitting input Matrixes, into 4 submatrices each.
         for (int i = 0; i < n / 2; i++)
@@ -164,41 +176,41 @@ void Mat<T>::Strassen(int N, T **A, T **B, T **C)
             }
         }
         //m1[][]
-        ADD(a11, a22, resa, halfsize);
-        ADD(b11, b22, resb, halfsize);
-        Strassen(halfsize, resa, resb, m1);
+        ADD(a11, a22, resa, n_half);
+        ADD(b11, b22, resb, n_half);
+        Strassen(n_half, resa, resb, m1);
         //m2[][]
-        ADD(a21, a22, resa, halfsize);
-        Strassen(halfsize, resa, b11, m2);
+        ADD(a21, a22, resa, n_half);
+        Strassen(n_half, resa, b11, m2);
         //m3[][]
-        SUB(b12, b22, resb, halfsize);
-        Strassen(halfsize, a11, resb, m3);
+        SUB(b12, b22, resb, n_half);
+        Strassen(n_half, a11, resb, m3);
         //m4[][]
-        SUB(b21, b11, resb, halfsize);
-        Strassen(halfsize, a22, resb, m4);
+        SUB(b21, b11, resb, n_half);
+        Strassen(n_half, a22, resb, m4);
         //m5[][]
-        ADD(a11, a12, resa, halfsize);
-        Strassen(halfsize, resa, b22, m5);
+        ADD(a11, a12, resa, n_half);
+        Strassen(n_half, resa, b22, m5);
         //m6[][]
-        SUB(a21, a11, resa, halfsize);
-        ADD(b11, b12, resb, halfsize);
-        Strassen(halfsize, resa, resb, m6);
+        SUB(a21, a11, resa, n_half);
+        ADD(b11, b12, resb, n_half);
+        Strassen(n_half, resa, resb, m6);
         //m7[][]
-        SUB(a12, a22, resa, halfsize);
-        ADD(b21, b22, resb, halfsize);
-        Strassen(halfsize, resa, resb, m7);
+        SUB(a12, a22, resa, n_half);
+        ADD(b21, b22, resb, n_half);
+        Strassen(n_half, resa, resb, m7);
         //C11 = M1 + M4 - M5 + M7;
-        ADD(m1, m4, resa, halfsize);
-        SUB(m7, m5, resb, halfsize);
-        ADD(resa, resb, c11, halfsize);
+        ADD(m1, m4, resa, n_half);
+        SUB(m7, m5, resb, n_half);
+        ADD(resa, resb, c11, n_half);
         //C12 = M3 + M5;
-        ADD(m3, m5, c12, halfsize);
+        ADD(m3, m5, c12, n_half);
         //C21 = M2 + M4;
-        ADD(m2, m4, c21, halfsize);
+        ADD(m2, m4, c21, n_half);
         //C22 = M1 + M3 - M2 + M6;
-        ADD(m1, m3, resa, halfsize);
-        SUB(m6, m2, resb, halfsize);
-        ADD(resa, resb, c22, halfsize);
+        ADD(m1, m3, resa, n_half);
+        SUB(m6, m2, resb, n_half);
+        ADD(resa, resb, c22, n_half);
         //at this point , we have calculated the c11..c22 matrices, and now we are going to
         //put them together and make a unit matrix which would describe our resulting Matrix.
         //组合小矩阵到一个大矩阵
@@ -222,7 +234,7 @@ void Mat<T>::Strassen(int N, T **A, T **B, T **C)
         // 释放矩阵内存空间
         for (int i = 0; i < n; i++)
             delete[] a[i], b[i], c[i];
-        for (int i = 0; i < halfsize; i++)
+        for (int i = 0; i < n_half; i++)
         {
             delete[] a11[i], a12[i], a21[i], a22[i];
             delete[] b11[i], b12[i], b21[i], b22[i];
@@ -239,29 +251,16 @@ void Mat<T>::Strassen(int N, T **A, T **B, T **C)
     }
 }
 template <typename T>
-void Mat<T>::Fill(T **A, T **B, int len)
-{
-    for (int row = 0; row < len; row++)
-    {
-        for (int column = 0; column < len; column++)
-        {
-            A[row][column] = rand() % 5;
-            B[row][column] = rand() % 5;
-            //matrix2[row][column] = rand() % 2;//ba hazfe in khat 50% afzayeshe soorat khahim dasht
-        }
-    }
-}
-template <typename T>
-void Mat<T>::Print(T **A, int Size)
+void Mat<T>::Print(T **A, int N)
 {
     cout << endl;
-    for (int row = 0; row < Size; row++)
+    for (int i = 0; i < N; i++)
     {
-        for (int column = 0; column < Size; column++)
+        for (int j = 0; j < N; j++)
         {
 
-            cout << A[row][column] << "\t";
-            if ((column + 1) % ((Size)) == 0)
+            cout << A[i][j] << "\t";
+            if ((j + 1) % ((N)) == 0)
                 cout << endl;
         }
     }
