af9781a185da5cb02821d1d6ba4797e72142fcc9
HuYi <y-hu17@mails.tsinghua.edu.cn>
2020-03-26 21:58:55 +0800

    0

--- C/test.cpp
+++ C/test.cpp
@@ -3,13 +3,19 @@
  * @Author: HuYi
  * @Date: 2020-03-05 20:53:32
  * @LastEditors: HuYi
- * @LastEditTime: 2020-03-05 20:53:47
+ * @LastEditTime: 2020-03-25 19:38:40
  */
-#include <stdio.h>
+#include <cstdio>
 #include <windows.h>
-int main()
+using namespace std;
+void mian()
 {
-    printf("Hello World\n");
+    int i, c, a[5];
+    c = 1;
+    for (i = 1; i <= 5; i++)
+    {
+        a[i] = 0;
+    }
+    printf("%d\n", c);
     system("pause");
-    return 0;
 }
